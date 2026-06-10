25af8702682b6ffa6211edcb1f01e9214bcc0705
Alejandro D Leon <alejandroperez9474@gmail.com>
2026-06-09 19:24:36 -0600

    anidacion de footer

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -3,15 +3,15 @@ import {Soup} from 'lucide-react'
 export function NavBar(){
     return <header className="flex top-0 left-0 w-full z-50 justify-around p-3 glass fixed">
         <div id="logo" className="flex gap-6 font-bold">
-            <a href="#home" className="hover-foreground"><Soup/></a>
-            <a href="#home" className="hover-foreground">Restaurante</a>
+            <a href="#home" className="hover:text-background"><Soup/></a>
+            <a href="#home" className="hover:text-background">Restaurante</a>
         </div>
 
         <nav className="flex gap-6">
-            <a href="#home" className="hover-foreground">Inicio</a>
-            <a href="#" className="hover-foreground">Menú</a>
-            <a href="#about" className="hover-foreground">Sobre nosotros</a>
-            <a href="#" className="hover-foreground">Contacto</a>
+            <a href="#home" className="hover:text-background">Inicio</a>
+            <a href="#" className="hover:text-background">Menú</a>
+            <a href="#about" className="hover:text-background">Sobre nosotros</a>
+            <a href="#" className="hover:text-background">Contacto</a>
         </nav>
     </header>
 }
